24b4caebb2c42a42f73e0618b24e4e8731ab91ac
M.Diky Oktavianto <rootdiky@gmail.com>
2023-06-03 22:05:43 +0700

    Parallel Camera
    New Path
    "Camera Parallax"
    "Fix Bug Animation"
    "Fix Mekanik Jump"

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -12,7 +12,8 @@ public class Player : MonoBehaviour
     [Header("Jump")]
     public float jumpForce;
     public Transform groundcheck;
-    public bool isGrounded;
+    private bool isGrounded;
+    private bool doDoubleJump;
     public LayerMask groundFloor;
     // Start is called before the first frame update
     void Start()
@@ -26,12 +27,34 @@ public class Player : MonoBehaviour
     {
         //player movement
         playerRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * runSpeed, playerRigidbody.velocity.y);
+        //Flipping Player
+        if (playerRigidbody.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(2, 2, 2);
+        }
+        else if (playerRigidbody.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-2, 2, 2);
+        }
         //Check Point
         isGrounded = Physics2D.OverlapCircle(groundcheck.position, 0.1f, groundFloor);
         //Jumping
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && (isGrounded || doDoubleJump))
         {
+            if (isGrounded)
+            {
+                doDoubleJump = true;
+            }
+            else
+            {
+                doDoubleJump = false;
+                playerAnimator.SetTrigger("Double Jump");
+            }
             playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpForce);
         }
+
+
+        playerAnimator.SetBool("IsGrounded", isGrounded);
+        playerAnimator.SetFloat("Speed", Mathf.Abs(playerRigidbody.velocity.x));
     }
 }
